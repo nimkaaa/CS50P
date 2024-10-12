325ab902ebca956350b37e193ab143a36f078602
nimkaaa <94477159+nimkaaa@users.noreply.github.com>
2024-10-12 19:34:47 +0200

    Create saying.py

---
+++ Lectures/Week_4/saying.py
@@ -0,0 +1,15 @@
+def main():
+   hello("Nastya")
+   goodbye("Nastya")
+
+
+def hello(name):
+   print(f"Hello, {name}")
+
+
+def goodbye(name):
+   print(f"Goodbye, {name}")
+
+
+if __name__ == "__main__":
+   main()
